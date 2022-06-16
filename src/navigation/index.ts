c27ae730b171e58f33e597ea962026f0dc9d2cd3
Pavlo Huk <pavlo.huk@icloud.com>
2022-06-17 00:27:37 +0300

    added type-safe navigation

--- src/navigation/index.ts
+++ src/navigation/index.ts
@@ -1,2 +1,3 @@
 export { Navigation } from './utils';
+export { ScreenParams } from './types';
 export { ScreenName } from './constants';
--- src/navigation/utils/navigation.util.ts
+++ src/navigation/utils/navigation.util.ts
@@ -1,19 +1,22 @@
 import { createRef } from 'react';
-import { singleton } from 'tsyringe';
+import { container } from 'tsyringe';
 import { NavigationContainerRef } from '@react-navigation/native';
 
-import { ScreenName } from '../constants';
+import { ScreenParams } from '../types';
 
-@singleton()
-export class Navigation {
+class NavigationHelper<P extends Record<T, any>, T extends string = string> {
   masterRef = createRef<NavigationContainerRef<Record<string, object>>>();
   detailsRef = createRef<NavigationContainerRef<Record<string, object>>>();
 
-  navigate(screenName: ScreenName): void {
+  navigate<N extends T>(screenName: N, params = {} as P[N]): void {
     if (this.detailsRef.current) {
-      this.detailsRef.current.navigate(screenName, {});
+      this.detailsRef.current.navigate(screenName, params);
     } else {
-      this.masterRef.current?.navigate(screenName, {});
+      this.masterRef.current?.navigate(screenName, params);
     }
   }
 }
+
+export class Navigation extends NavigationHelper<ScreenParams> {}
+
+container.register(Navigation, { useValue: new Navigation() });
